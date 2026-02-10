b7616ce22cd0b53e65cd814cba2eaf1ffef86c7b
annezhang88888-prog <annezhang88888@gmail.com>
2026-02-09 21:44:41 -0800

    Add self introduction to lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,6 @@ def main():
     name = "Anne Zhang" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
-
+    print("My name is Anne Zhang. I am a 9th grader from California")
 if __name__ == "__main__":
     main()
